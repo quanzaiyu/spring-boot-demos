a2dd692e7fefda10e61d916cf463629bc0fba00b
quanzaiyu <731734107@qq.com>
2019-09-16 16:16:01 +0800

    提取依赖

--- spring-boot-banner/src/main/java/com/example/mail/banner/BannerApplication.java
+++ spring-boot-banner/src/main/java/com/example/mail/banner/BannerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BannerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BannerApplication.class, args);
     }
