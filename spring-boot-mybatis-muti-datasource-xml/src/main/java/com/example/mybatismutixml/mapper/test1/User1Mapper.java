b2bd38428d59bb7c6886954f2e9d06b056a70261
quanzaiyu <731734107@qq.com>
2019-09-19 09:49:01 +0800

    muti-database-xml

---
+++ spring-boot-mybatis-muti-datasource-xml/src/main/java/com/example/mybatismutixml/mapper/test1/User1Mapper.java
@@ -0,0 +1,15 @@
+package com.example.mybatismutixml.mapper.test1;
+
+import com.example.mybatismutixml.model.User;
+
+import java.util.List;
+
+public interface User1Mapper {
+
+    List<User> getAll();
+    User getOne(Long id);
+    void insert(User user);
+    void update(User user);
+    void delete(Long id);
+
+}
---
+++ spring-boot-mybatis-muti-datasource-xml/src/test/java/com/example/mybatismuti/mapper/test1/User1MapperTest.java
@@ -0,0 +1,9 @@
+package com.example.mybatismuti.mapper.test1;
+
+import org.junit.Test;
+
+public class User1MapperTest {
+    @Test
+    public void getAll() {
+    }
+}
