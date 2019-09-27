798aae2e88434a753050355a8e2f3a320a06c06a
quanzaiyu <731734107@qq.com>
2019-09-27 09:27:25 +0800

    🎨 热更新

--- spring-boot-banner/src/test/java/com/example/banner/BannerTests.java
+++ spring-boot-banner/src/test/java/com/example/banner/BannerTests.java
@@ -1,5 +1,6 @@
 package com.example.banner;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -7,4 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BannerTests {
+    @Test
+    public void contextLoads() {
+    }
 }
