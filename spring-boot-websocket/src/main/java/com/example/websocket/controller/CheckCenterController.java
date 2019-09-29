6b17ca8314d349a39c78225088f987892b60b312
quanzaiyu <731734107@qq.com>
2019-09-29 14:45:22 +0800

    ✨ websocket

---
+++ spring-boot-websocket/src/main/java/com/example/websocket/controller/CheckCenterController.java
@@ -0,0 +1,52 @@
+package com.example.websocket.controller;
+
+import com.example.websocket.component.WebSocketServer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/checkcenter")
+public class CheckCenterController {
+
+    // 页面请求
+    @GetMapping("/socket/{cid}")
+    public String index(ModelMap map, @PathVariable String cid) {
+        map.addAttribute("cid", cid);
+        return "socket";
+    }
+
+    // 推送数据接口
+    @ResponseBody
+    @RequestMapping("/socket/push/{from}/{to}")
+    public Result pushToOne(@PathVariable String from, @PathVariable String to, String message) {
+        System.out.println("one");
+        try {
+            WebSocketServer.send(message, from, to);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Result.error(from + " : " + to + "#" + e.getMessage());
+        }
+        return Result.ok("发送成功");
+    }
+
+    // 推送数据接口
+    @ResponseBody
+    @RequestMapping("/socket/push/{from}")
+    public Result pushToAll(@PathVariable String from, String message) {
+        System.out.println("all");
+        try {
+            WebSocketServer.send(message, from, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Result.error(from + " : " + null + "#" + e.getMessage());
+        }
+        return Result.ok("发送成功");
+    }
+}
---
+++ spring-boot-websocket/src/main/java/com/example/websocket/controller/Result.java
@@ -0,0 +1,22 @@
+package com.example.websocket.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result {
+    private Object data;
+    private Integer status;
+    private String msg;
+
+    public static Result ok(Object data){
+        return new Result(data, 1, "数据请求成功");
+    }
+
+    public static Result error(String msg){
+        return new Result("", 0, msg);
+    }
+}
