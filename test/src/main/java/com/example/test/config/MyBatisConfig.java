85db68a1b921bf89046f1819f34cb5f45d92d3e3
quanzaiyu <731734107@qq.com>
2020-08-05 09:29:43 +0800

    💗 0

--- test/src/main/java/com/example/test/config/MyBatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan({"com.example.test.mdb.mapper"})
-public class MyBatisConfig {
-}
